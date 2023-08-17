b16ac14007e3467a3a31d1a40794cb86c3d12bec
sathish24499 <sathish24499@gmail.com>
2023-08-17 17:08:34 +0530

    push from qa

--- src/org/fjhij/SecondpushintoGit.java
+++ src/org/fjhij/SecondpushintoGit.java
@@ -4,6 +4,8 @@ public class SecondpushintoGit {
 
 	public static void main(String[] args) {
 System.out.println("my second push and first in second push");
+System.out.println("tester zone");
+System.out.println("fine working");
 	}
 
 }
